5e0bd9a9b255dd1ea95e6a654cc13a49e4c3f812
Mzwandile Dladla <mzwandile.dladla@siyakha.co.za>
2020-06-15 13:10:33 +0200

    diplayed list of heroes using a class object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+            <h1>{{title}}</h1>
+            <h2>My favourite hero is : {{myHero}}</h2>
+            <p>Heroes :</p>
+    <ul>
+    <li *ngFor=" let hero of heroes">{{hero.name}}</li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'display-data';
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(2, 'Bombasto'),
+    new Hero(3, 'Magneta'),
+    new Hero(4, 'Tornado')
+  ];
+
+  myHero = this.heroes[0].name;
 }
